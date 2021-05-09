bb0f1c2e59c095643d97c4e7ac035181c0ccac1d
Tetsu Haruyama <tetsu.yes@gmail.com>
2021-05-09 14:44:44 +0900

    0.1.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,1 +1,2 @@
 v0.1.1, 2021-05-09 -- Initial release.
+v0.1.2, 2021-05-09 -- Github Action test.
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ for filename in glob.iglob('./py4macro/**', recursive=True):
 
 setup(
     name='py4macro',
-    version='0.1.1',
+    version='0.1.2',
     author='Tetsu Haruyama',
     author_email='haruyama@econ.kobe-u.ac.jp',
     packages=find_packages(exclude=("data_generation",)),
