b3b3a7837e82eea64b4e4690f906ad7248d461eb
Tetsu HARUYAMA <tetsu@T-MBP3.local>
2022-11-03 11:15:53 +0900

    price and deflator added

--- py4macro/__init__.py
+++ py4macro/__init__.py
@@ -7,19 +7,19 @@ from .py4macro import *
 __all__ = ['data','trend','show']
 
 __author__ = 'Tetsu Haruyama'
-__version__ = '0.6.1'
+__version__ = '0.7.0'
 __copyright__ = 'Copyright (c) 2022 Tetsu Haruyama'
 
 __doc__ = """
     「Pythonで学ぶマクロ経済学 (中級＋レベル)」のためのモジュール
 
-        * HPフィルター関数
+        * HPフィルターを使いトレンドを抽出するtrend関数
         * DataFrameを全て表示するshow関数
         * データ・セット
             * Penn World Tables 10.0
             * IMF World Economic Outlook 2021
             * Maddison Project Database 2020
             * 日本の四半期データ（GDPなど）
-            * 日本の四半期データ（マネーストックなど）
+            * 日本の月次データ（マネーストックなど）
             * 177ヵ国のマネーストックなど
             * 円/ドル為替レート"""
