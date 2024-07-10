7ec69b200beae0d3f0b5244b6440fb6c67fc1d38
Tetsu Haruyama <tetsu.yes@gmail.com>
2024-07-10 15:38:24 +0900

    0.8.11

--- py4macro/__init__.py
+++ py4macro/__init__.py
@@ -7,7 +7,7 @@ from .py4macro import *
 __all__ = ['data','trend','show','xvalues','recessions','fukyo', 'see']
 
 __author__ = 'Tetsu Haruyama'
-__version__ = '0.8.11a'
+__version__ = '0.8.11'
 __copyright__ = 'Copyright (c) 2024 Tetsu Haruyama'
 
 __doc__ = """
